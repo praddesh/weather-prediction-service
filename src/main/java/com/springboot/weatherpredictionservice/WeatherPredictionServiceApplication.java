469f88a5a17bf6399ed4db27c8eebdd5e69a9386
praddesh <pradeep.acky@gmail.com>
2023-08-22 10:53:26 +0530

    Initial Code Changes

---
+++ src/main/java/com/springboot/weatherpredictionservice/WeatherPredictionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.weatherpredictionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherPredictionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherPredictionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/weatherpredictionservice/WeatherPredictionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.weatherpredictionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherPredictionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
